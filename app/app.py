a20b1916976bfe3d4e39872bc0dc4e56c8c66bff
Pavel Shapka <shapka.pa@phystech.edu>
2024-11-16 19:49:13 +0300

    add sub func

--- app/app.py
+++ app/app.py
@@ -1,3 +1,5 @@
 def add(a: float, b: float) -> float:
     return a + b
 
+def add(a: float, b: float) -> float:
+    return a - b
--- app/app_test.py
+++ app/app_test.py
@@ -2,3 +2,6 @@ from app import add
 
 def test_add():
     assert add(5., 7.) == 12.
+
+def test_sub():
+    assert add(5., 7.) == -2.
