f3d194db29c52df78942b0f4158b42dfcb67d1e0
Pavel Shapka <shapka.pa@phystech.edu>
2024-11-17 13:20:37 +0300

    add mul

--- app/app.py
+++ app/app.py
@@ -3,3 +3,6 @@ def add(a: float, b: float) -> float:
 
 def sub(a: float, b: float) -> float:
     return a - b
+
+def mul(a: float, b: float) -> float:
+    return a * b
--- app/app_test.py
+++ app/app_test.py
@@ -1,7 +1,10 @@
-from app import add, sub
+from app import add, sub, mul
 
 def test_add():
     assert add(5., 7.) == 12.
 
 def test_sub():
     assert sub(5., 7.) == -2.
+
+def test_mul():
+    assert mul(5., 7.) == 35.
