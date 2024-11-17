850b2703e3fc5f254659c04880a1d11d5b672338
Pavel Shapka <shapka.pa@phystech.edu>
2024-11-17 14:34:15 +0300

    unittest

--- app/app.py
+++ app/app.py
@@ -1,8 +1,31 @@
-def add(a: float, b: float) -> float:
-    return a + b
+from typing import Self
 
-def sub(a: float, b: float) -> float:
-    return a - b
+class Calculator():
+    def __init__(self) -> None:
+        pass
 
-def mul(a: float, b: float) -> float:
-    return a * b
+    def add(self, a: float, b: float) -> float:
+        return a + b
+
+    def sub(self, a: float, b: float) -> float:
+        return a - b
+
+    def mul(self, a: float, b: float) -> float:
+        return a * b
+    
+class String_builder():
+    def __init__(self, buffer: None | str = None) -> None:
+        if buffer is None:
+            buffer = ""
+        self.buffer = [buffer]
+
+    def add_after(self, new: str) -> Self:
+        self.buffer.append(new)
+        return self
+
+    def add_before(self, new: str)-> Self:
+        self.buffer = [new] + self.buffer
+        return self
+
+    def build(self) -> str:
+        return ''.join(self.buffer)
---
+++ tests/test_string_builder.py
@@ -0,0 +1,18 @@
+import unittest
+from app import app
+
+
+class Test_string_builder(unittest.TestCase):
+    def setUp(self) -> None:
+        self.string_builder = app.String_builder()
+    
+    def test_add(self):
+        self.string_builder.add_after("Hello")
+        self.assertEqual(self.string_builder.add_after(", World!").build(), "Hello, World!")
+
+    def test_sub(self):
+        self.string_builder.add_after("Hello")
+        self.assertEqual(self.string_builder.add_before(", World!").build(), ", World!Hello")
+
+if __name__ == "__main__":
+    unittest.main()
